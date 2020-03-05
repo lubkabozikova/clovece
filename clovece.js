a46bbb60dd8c35b2f61f4223867e36cf433a840e
Lubka Bozikova <lubkap@gmail.com>
2020-03-05 23:03:02 +0100

    correct creating SVG in javascript

--- clovece.js
+++ clovece.js
@@ -1,25 +1,25 @@
 var divHra = document.createElement("div");
-    divHra.className = "divHra";
-
-// var svgHra = document.createElement("svg");
-//     svgHra.className = "svgHra";
-
-// var planCircle = document.createElement("circle");
-//     // planCircle.className = "plan";
-//     planCircle.cx = "10";
-//     planCircle.cy = "10";
-//     planCircle.r = "5";
+divHra.className = "divHra";
     
+const SVG_NS = 'http://www.w3.org/2000/svg';
+var svgHra = document.createElementNS(SVG_NS, "svg");
+svgHra.className = "svgHra";
 
+var planCircle = document.createElementNS(SVG_NS, "circle");
+    // // planCircle.className = "plan";
+    // planCircle.cx = "10";
+    // planCircle.cy = "10";
+    // planCircle.r = "5";
 
 document.body.appendChild(divHra);
-// divHra.appendChild(svgHra);
+divHra.appendChild(svgHra);
+
 // svgHra.setAttribute("width", "100");
 // svgHra.setAttribute("height", "100");
-// svgHra.appendChild(planCircle);
-// planCircle.setAttribute("cx", "10");
-// planCircle.setAttribute("cy", "10");
-// planCircle.setAttribute("r", "5");
-// planCircle.setAttribute("fill", "white");
-// planCircle.setAttribute("stroke", "black");
-// planCircle.setAttribute("stroke-width", "2");
+svgHra.appendChild(planCircle);
+planCircle.setAttribute("cx", "10");
+planCircle.setAttribute("cy", "10");
+planCircle.setAttribute("r", "5");
+planCircle.setAttribute("fill", "white");
+planCircle.setAttribute("stroke", "black");
+planCircle.setAttribute("stroke-width", "2");
