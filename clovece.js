d3bd0d66aa2b941374b062b66039faab0b63e3e0
Daniel Bozik <daniel.bozik@gmail.com>
2020-03-05 22:00:31 +0100

    init

---
+++ clovece.js
@@ -0,0 +1,25 @@
+var divHra = document.createElement("div");
+    divHra.className = "divHra";
+
+// var svgHra = document.createElement("svg");
+//     svgHra.className = "svgHra";
+
+// var planCircle = document.createElement("circle");
+//     // planCircle.className = "plan";
+//     planCircle.cx = "10";
+//     planCircle.cy = "10";
+//     planCircle.r = "5";
+    
+
+
+document.body.appendChild(divHra);
+// divHra.appendChild(svgHra);
+// svgHra.setAttribute("width", "100");
+// svgHra.setAttribute("height", "100");
+// svgHra.appendChild(planCircle);
+// planCircle.setAttribute("cx", "10");
+// planCircle.setAttribute("cy", "10");
+// planCircle.setAttribute("r", "5");
+// planCircle.setAttribute("fill", "white");
+// planCircle.setAttribute("stroke", "black");
+// planCircle.setAttribute("stroke-width", "2");
