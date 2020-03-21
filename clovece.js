b30f94e7efaa8dfa63b3f46abea03db92cc1cfc8
Lubka Bozikova <lubkap@gmail.com>
2020-03-21 13:25:15 +0100

    domcek

--- clovece.js
+++ clovece.js
@@ -1,31 +1,36 @@
 var divHra = document.createElement("div");
 divHra.className = "divHra";
-    
+
+const SVG_NS = 'http://www.w3.org/2000/svg';
+
 var Centers = [
-    "50,290","110,290","170,290","230,290","290,290",
-    "290,230","290,170","290,110","290,50",
-    "350,50","410,50",
-    "410,110","410,170","410,230","410,290",
-    "470,290","530,290","590,290","650,290",
-    "650,350","650,410",
-    "590,410","530,410","470,410","410,410",
-    "410,470","410,530","410,590","410,650",
-    "350,650","290,650",
-    "290,590","290,530","290,470","290,410",
-    "230,410","170,410","110,410","50,410",
-    "50,350"];
-// var CentersRed = []
-// var CentersGreen
-// var CentersBlue
-// var CentersYellow
-// var Homes
+    "50,290","110,290","170,290","230,290",
+    "290,290","290,230","290,170","290,110",
+    "290,50","350,50",
+    "410,50","410,110","410,170","410,230",
+    "410,290","470,290","530,290","590,290",
+    "650,290","650,350",
+    "650,410","590,410","530,410","470,410",
+    "410,410","410,470","410,530","410,590",
+    "410,650","350,650",
+    "290,650","290,590","290,530","290,470",
+    "290,410","230,410","170,410","110,410",
+    "50,410","50,350"
+];
+var Colors = ["red","green","blue","yellow"];
+var Homes = ["110,180","520,110","590,520","180,590"];
+var SpecialCenters = [
+    ["50,290","110,350","170,350","230,350","290,350"],
+    ["410,50","350,110","350,170","350,230","350,290"],
+    ["650,410","590,350","530,350","470,350","410,350"],
+    ["290,650","350,590","350,530","350,470","350,410"]
+];
 
 var polygonPoints = Centers[0];
 for (let index = 1; index < Centers.length; index++) {
     polygonPoints = polygonPoints + " " + Centers[index];
     }
 
-const SVG_NS = 'http://www.w3.org/2000/svg';
 var svgHra = document.createElementNS(SVG_NS, "svg");
 svgHra.className = "svgHra";
     svgHra.setAttribute("height","100%");
@@ -40,18 +45,35 @@ var planCiara = document.createElementNS(SVG_NS, "polygon");
     planCiara.setAttribute("stroke","black");
     planCiara.setAttribute("stroke-width","3");
 
+function newCircle(cx, cy, r, fill){
+    let planCircle = document.createElementNS(SVG_NS, "circle");
+        planCircle.setAttribute("cx",cx);
+        planCircle.setAttribute("cy",cy);
+        planCircle.setAttribute("r",r);
+        planCircle.setAttribute("fill", fill);
+        planCircle.setAttribute("stroke", "black");
+        planCircle.setAttribute("stroke-width", "3");
+    svgHra.appendChild(planCircle);
+}
+
 svgHra.appendChild(planCiara);
 
 for (let index = 0; index < Centers.length; index++) {
     let X = Centers[index].split(",");
-    let planCircle = document.createElementNS(SVG_NS, "circle");
-        planCircle.setAttribute("cx",X[0]);
-        planCircle.setAttribute("cy",X[1]);
-        planCircle.setAttribute("r","25");
-        planCircle.setAttribute("fill", "white");
-        planCircle.setAttribute("stroke", "black");
-        planCircle.setAttribute("stroke-width", "3");
+    newCircle(X[0],X[1],"25","white");
+}
 
-    svgHra.appendChild(planCircle);
+for (let index = 0; index < Colors.length; index++) {
+    let X = Homes[index].split(",");
+    newCircle(X[0],X[1],"70",Colors[index]);
+    const a = 28;
+    newCircle((+X[0] -a).toString(),(+X[1] -a).toString(),"25","white");
+    newCircle((+X[0] -a).toString(),(+X[1] +a).toString(),"25","white");
+    newCircle((+X[0] +a).toString(),(+X[1] -a).toString(),"25","white");
+    newCircle((+X[0] +a).toString(),(+X[1] +a).toString(),"25","white");
+    for (let j = 0; j < SpecialCenters[index].length; j++) {
+        X = SpecialCenters[index][j].split(",");
+        newCircle(X[0],X[1],"25",Colors[index]);
+    }
 }
 
